ddf75fb11f37b5f8baee51181989105075bb5495
Athul Anoop <athulanoop2020@gmail.com>
2024-10-10 23:00:54 +0530

    enhanced: styling and added toggle for theme

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,24 @@
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "../store/ThemeProvider";
+import { Transition, TransitionChild } from "@headlessui/react";
+import { useState } from "react";
+
 const links = [
-  { name: "Home", href: "/" },
+  { name: "Chat", href: "/chat" },
   { name: "Meditation", href: "/meditation" },
   { name: "Articles", href: "/articles" },
 ];
 
 export default function Nav() {
+  const { theme, setTheme } = useTheme();
+  const [darkTransitionFinished, setDarkTransitionFinished] = useState(false);
+  const [lightTransitionFinished, setLightTransitionFinished] = useState(true);
   return (
-    <nav className="w-full h-10 border rounded-full">
-      <ul className="px-4 flex justify-around items-center w-full h-full">
+    <nav className="w-full h-10 px-4 flex justify-between items-center shadow-xl rounded-full bg-neutral-50 dark:bg-neutral-900 transition duration-100 ease-in-out">
+      <span className="px-2 shrink-0 text-lg font-bold bg-teal-400 shadow-md shadow-teal-500/50 rounded-full">
+        <a href="/">Calm Space</a>
+      </span>
+      <ul className="flex justify-around items-center w-full h-full">
         {links.map((link, index) => (
           <li
             className="px-2 py-1 flex items-center hover:bg-teal-300 transition ease-in-out duration-300 rounded-full"
@@ -17,6 +28,31 @@ export default function Nav() {
           </li>
         ))}
       </ul>
+      <button
+        className={`my-3 p-1 ${theme === "dark" ? "text-white" : ""} rounded-lg transition duration-100 ease-in`}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      >
+        <Transition
+          show={theme === "dark" && lightTransitionFinished}
+          beforeEnter={() => setDarkTransitionFinished(false)}
+          afterLeave={() => setDarkTransitionFinished(true)}
+        >
+          <Moon
+            size={24}
+            className="transition duration-100 ease-in-out data-[closed]:opacity-0"
+          />
+        </Transition>
+        <Transition
+          show={theme === "light" && darkTransitionFinished}
+          beforeEnter={() => setLightTransitionFinished(false)}
+          afterLeave={() => setLightTransitionFinished(true)}
+        >
+          <Sun
+            size={24}
+            className="transition duration-100 ease-in-out data-[closed]:opacity-0"
+          />
+        </Transition>
+      </button>
     </nav>
   );
 }
