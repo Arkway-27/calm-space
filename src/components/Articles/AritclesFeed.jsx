d5caa185940715d937e90a46725b8c44f39d7607
Athul Anoop <athulanoop2020@gmail.com>
2024-10-12 17:54:26 +0530

    fix: deployment resource fetch

--- src/components/Articles/AritclesFeed.jsx
+++ src/components/Articles/AritclesFeed.jsx
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ArticlesFeed = () => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetch('public/data/articles.json')
+    fetch("/data/articles.json")
       .then((response) => response.json())
       .then((data) => setArticles(data))
-      .catch((error) => console.error('Error fetching articles:', error));
+      .catch((error) => console.error("Error fetching articles:", error));
   }, []);
 
   return (
@@ -17,9 +17,14 @@ const ArticlesFeed = () => {
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
         {articles.map((article) => (
-          <div key={article.id} className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col">
+          <div
+            key={article.id}
+            className="bg-white rounded-lg shadow-md overflow-hidden flex flex-col"
+          >
             <div className="p-6">
-              <h3 className="text-xl font-semibold text-blue-800 mb-2">{article.title}</h3>
+              <h3 className="text-xl font-semibold text-blue-800 mb-2">
+                {article.title}
+              </h3>
               <p className="text-sm text-red-600 mb-4">{article.author}</p>
               <p className="text-gray-700 mb-4">{article.excerpt}</p>
             </div>
