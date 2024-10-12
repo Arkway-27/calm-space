123cab4ec3b22ba2047061bfd5613912f80983dc
Roshin R <139961141+RoshinR2005@users.noreply.github.com>
2024-10-12 17:28:13 +0530

    Added articles.json

--- src/pages/Articles.jsx
+++ src/pages/Articles.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import ArticlesFeed from "../components/Articles/AritclesFeed";
+
 export default function Articles() {
   return (
     <>
       <h1 className="text-3xl font-bold">Articles</h1>
-      <p className="text-lg">Articles about calm and peace</p>
+      <ArticlesFeed/>
     </>
   );
 }
